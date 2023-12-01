a9b316dbe07dfc5cab90bde5515a57690e3a1d25
paramthakkar123 <paramthakkar864@gmail.com>
2023-12-01 15:33:55 +0530

    All tasks done

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  const length = prompt("Enter password length (8-128 characters):");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  if (length === null || length === "" || isNaN(length)) {
+    alert("Please enter a valid password length.");
+    return;
+  }
 
-  passwordText.value = password;
+  const passwordLength = parseInt(length);
 
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+    return;
+  }
+
+  const includeUppercase = confirm("Include uppercase letters?");
+  const includeLowercase = confirm("Include lowercase letters?");
+  const includeNumbers = confirm("Include numbers?");
+  const includeSpecialChars = confirm("Include special characters?");
+
+  let charset = "";
+  let password = "";
+
+  if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+  if (includeNumbers) charset += "0123456789";
+  if (includeSpecialChars) charset += "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+  if (charset === "") {
+    alert("Please select at least one character type.");
+    return;
+  }
+
+  for (let i = 0; i < passwordLength; ++i) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  document.getElementById("password").value = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
