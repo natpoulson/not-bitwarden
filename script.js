0b85b467491006f27747e73852938c2c2825caf6
Nat Poulson <nat.poulson@outlook.com.au>
2023-12-02 21:27:31 +1100

    Scaffold out configuration method and build out setters for remaining properties

--- script.js
+++ script.js
@@ -16,17 +16,29 @@ class Setting {
 
   init() {
     this._length = Setting.default.minLength;
-    this.useUpperCase = Setting.default.toggle;
-    this.useNumbers = Setting.default.toggle;
-    this.useSymbols = Setting.default.toggle;
+    this._useUpperCase = Setting.default.toggle;
+    this._useNumbers = Setting.default.toggle;
+    this._useSymbols = Setting.default.toggle;
   }
 
-  // Getter and Setter for the length property
+  // Getters
   get length() {
     return this._length;
   }
 
-  set length( value ) {
+  get useUpperCase() {
+    return this._useUpperCase;
+  }
+
+  get useNumbers() {
+    return this._useNumbers;
+  }
+
+  get useSymbols() {
+    return this._useSymbols;
+  }
+
+  set length(value) {
     // Vet any values that are below minimum length, aren't numbers, or evaluate to Not a Number (NaN), which is - ironically - of type number
     if ( (value < Setting.minLength) || (typeof value !== 'number') || (value === NaN) ) {
       return Setting.minLength;
@@ -40,22 +52,75 @@ class Setting {
     // By this point all invalidating criteria should be accounted for
     return value;
   }
+
+  set useUpperCase(value) {
+    if (Setting.checkBool(value) !== null) {
+      this._setUpperCase = value;
+    }
+  }
+
+  set useNumbers(value) {
+    if (Setting.checkBool(value) !== null) {
+      this._useNumbers = value;
+    }
+  }
+
+  set useSymbols(value) {
+    if (Setting.checkBool(value) !== null) {
+      this._useSymbols = value;
+    }
+  }
+
+  static checkBool(value) {
+    if (typeof value === 'boolean') {
+      return value;
+    }
+    // Returns null if it isn't a boolean so we know to toss it
+    return null;
+  }
+
+  static isEnabled(parameter) {
+    if (typeof parameter !== 'boolean') {
+      return undefined;
+    }
+    if (parameter) {
+      return 'yes';
+    }
+    return 'no';
+  }
+
+  configure() {
+    // Show defaults and ask if they would like to use them
+    if (window.confirm(`These are your current settings:\n
+    Password Length: ${this.length}
+    Use Uppercase: ${Setting.isEnabled(this.useUpperCase)}
+    Use Numbers: ${Setting.isEnabled(this.useNumbers)}
+    Use Symbols: ${Setting.isEnabled(this.useSymbols)}\n
+    Do you want to change these settings?`)) {
+      // Ask for the number of characters they want
+      
+      // Ask if they want uppercase characters
+
+      // Ask if they want numbers
+    
+      // Ask if they want special characters
+
+    }
+
+
+  }
 }
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // Assigning the button in form to variable
 
 // Initialise an settings object with properties representing the options selected
-const settings = {
-  length: 8,
-  useUpperCase: false,
-  useNumbers: false,
-  useSymbols: false
-};
+const settings = new Setting;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); // Calling non-existent function called generatePassword()
+  settings.configure(); // Invoke config method before running the generator
+  var password = generatePassword(settings); // Calling non-existent function called generatePassword()
   var passwordText = document.querySelector("#password"); // Assigning output area to the variable
 
   passwordText.value = password; // Writing the password to the output area using said variable.
@@ -68,4 +133,4 @@ generateBtn.addEventListener("click", writePassword); // Establishing event list
 // Planning
 // Add a function to prompt the user for input and then write the config to the settings object
 // Add validator functions/methods to ensure parameters are valid
-// Add generatePassword using params fed from the settings object (maybe we should use class syntax?)
+// Add generatePassword using params fed from the settings object (maybe we should use class syntax?)h
